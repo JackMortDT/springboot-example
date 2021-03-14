d9edc4c10579d826a754be9bca696bec7ee16aad
Luisito <le.sastre@ebc.edu.mx>
2021-03-13 20:28:23 -0600

    Adding logic for person service

--- src/main/groovy/com/example/demo/service/PersonService.groovy
+++ src/main/groovy/com/example/demo/service/PersonService.groovy
@@ -2,17 +2,25 @@ package com.example.demo.service
 
 import com.example.demo.domain.Person
 import com.example.demo.repository.PersonRepository
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import com.example.demo.command.PersonCommand
 
+@Slf4j
 @Service
 class PersonService {
 
   @Autowired
   PersonRepository personRepository
 
-  List<Person> getPeople() {
-    personRepository.findAll()
+  @Transactional(readOnly = true)
+  List<PersonCommand> getPeople() {
+    List<Person> people = personRepository.findAll()
+    people.collect() { person ->
+      PersonCommand.toPersonCommand(person)
+    }
   }
 
 }
