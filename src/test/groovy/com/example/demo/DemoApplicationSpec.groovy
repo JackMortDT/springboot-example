3332dc0fd0089036e459dd008a49b80580e552cf
Luisito <le.sastre@ebc.edu.mx>
2021-03-13 20:26:03 -0600

    Renaming application test to application spec

--- src/test/groovy/com/example/demo/DemoApplicationSpec.groovy
+++ src/test/groovy/com/example/demo/DemoApplicationSpec.groovy
@@ -1,13 +1,9 @@
 package com.example.demo
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
 
 @SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class DemoApplicationSpec extends Specification {
 
 }
