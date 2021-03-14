47cc57702053e3fa5716563c5ba541d812518b97
Luisito <le.sastre@ebc.edu.mx>
2021-03-13 20:26:50 -0600

    Addding parse person to personCommand

--- src/main/groovy/com/example/demo/command/PersonCommand.groovy
+++ src/main/groovy/com/example/demo/command/PersonCommand.groovy
@@ -1,10 +1,21 @@
 package com.example.demo.command
 
+import com.example.demo.domain.Person
+
 class PersonCommand {
 
   String name
   String lastName
   Integer age
   String fullName
+
+  static PersonCommand toPersonCommand(Person person) {
+    new PersonCommand(
+      name: person.name,
+      lastName: person.lastName,
+      age: person.age,
+      fullName: "${person.name} ${person.lastName}"
+    )
+  }
 
 }
